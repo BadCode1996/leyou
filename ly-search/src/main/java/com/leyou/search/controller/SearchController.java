62346652b3359bf27ae322a932852bc63d4ae289
SRD <34228449+BadCode1996@users.noreply.github.com>
2020-07-27 16:22:58 +0800

    Update SearchController.java

--- ly-search/src/main/java/com/leyou/search/controller/SearchController.java
+++ ly-search/src/main/java/com/leyou/search/controller/SearchController.java
@@ -27,29 +27,4 @@ public class SearchController {
         }
         return ResponseEntity.ok(goodsPageResult);
     }
-
-
-    /**
-     * http://api.leyou.com/api/search/test1?key=123
-     *
-     * @param key id
-     * @return null
-     */
-    @PostMapping("test1")
-    public ResponseEntity<PageResult<Goods>> test1(@RequestParam String key) {
-        System.out.println("key = " + key);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    /**
-     * http://api.leyou.com/api/search/test2/123
-     *
-     * @param id id
-     * @return null
-     */
-    @PostMapping("test2/{id}")
-    public ResponseEntity<PageResult<Goods>> test2(@PathVariable("id") Long id) {
-        System.out.println("id = " + id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 }
